35489108d6949e82927e075c3276f34ad289cdcc
dkrym <dkrym@users.noreply.github.com>
2020-08-17 10:28:37 +1000

    Search widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
   { title: 'What is React?', content: 'React is a front end js framework' },
@@ -10,7 +11,7 @@ const items = [
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
