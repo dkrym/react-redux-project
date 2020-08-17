89afef00dfbbe8e49689e9c4c801e1630262a37e
dkrym <dkrym@users.noreply.github.com>
2020-08-17 13:54:58 +1000

    Translate widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
   { title: 'What is React?', content: 'React is a front end js framework' },
@@ -16,21 +17,9 @@ const options = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
-
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>
-        Toggle Dropdown
-      </button>
-      {showDropdown ? (
-        <Dropdown
-          options={options}
-          selected={selected}
-          onSelectedChange={setSelected}
-        />
-      ) : null}
+      <Translate />
     </div>
   );
 };
