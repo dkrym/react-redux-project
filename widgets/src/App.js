f13c6d7543e381388767bdb2f826a3b8b97b8e2f
dkrym <dkrym@users.noreply.github.com>
2020-08-17 12:53:55 +1000

    Dropbox widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
   { title: 'What is React?', content: 'React is a front end js framework' },
@@ -8,10 +9,28 @@ const items = [
   { title: 'Where React?', content: 'In your browser' }
 ];
 
+const options = [
+  { label: 'The Color Red', value: 'red' },
+  { label: 'The Color Green', value: 'green' },
+  { label: 'The Shade of Blue', value: 'blue' }
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
-      <Search />
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      ) : null}
     </div>
   );
 };
